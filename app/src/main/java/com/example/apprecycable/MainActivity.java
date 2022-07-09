41e23067c2e23b4dfd0cfb83421ee3d21d0dfdc8
Dan Santana <2019030386@upsin.edu.mx>
2022-07-08 20:13:07 -0600

    Se añadieron nuevas clases y modificaciones para la evidencia 2 del Corte 2, incluyendo altas y modicaciones | SQLite (Base de datos)

--- app/src/main/java/com/example/apprecycable/MainActivity.java
+++ app/src/main/java/com/example/apprecycable/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private FloatingActionButton fbtnAgregar;
+    private Aplicacion app;
     private Alumno alumno;
     private int posicion = -1;
 
@@ -30,20 +32,42 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         fbtnAgregar = (FloatingActionButton) findViewById(R.id.agregarAlumno);
 
-        app.getAdaptador().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int posicion = recyclerView.getChildAdapterPosition(v);
-                String dato = app.getAlumnos().get(posicion).getNombre();
-                Toast.makeText(MainActivity.this, "Se hizo click en " + dato, Toast.LENGTH_SHORT).show();
-            }
-        });
-        
         fbtnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Uno nuevo", Toast.LENGTH_SHORT).show();
+                alumno = null;
+                Intent intent = new Intent(MainActivity.this, AlumnoAlta.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("alumno", alumno);
+                bundle.putInt("posicion", posicion);
+                intent.putExtras(bundle);
+
+                startActivityForResult(intent, 0);
             }
         });
+
+        app.getAdaptador().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                posicion = recyclerView.getChildAdapterPosition(v);
+                alumno = app.getAlumnos().get(posicion);
+
+                Intent intent = new Intent(MainActivity.this, AlumnoAlta.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("alumno", alumno);
+                intent.putExtra("posicion", posicion);
+                intent.putExtras(bundle);
+
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        recyclerView.getAdapter().notifyDataSetChanged();
+        posicion = -1;
     }
 }
